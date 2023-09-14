29886789c3f6465df403f8033296112de5688a12
Ahmet Ordulu <ahmet.ordulu@hotmail.se>
2023-09-15 00:03:07 +0200

    updated openai version

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const express = require('express');
-const path = require('path');
+import 'dotenv/config';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.PORT || 3000;
-require('dotenv').config();
-//Body parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+const port = 5500;
 
-//Static file
-app.use(express.static(path.join(__dirname)))
+// Body parser Middleware
+import bodyParser from 'body-parser';
+import cors  from 'cors';
+import OpenAi from 'openai';
 
-app.use('/openai', require('./routes/openaiRoutes'))
+const { OPENAI_API_KEY } = process.env;
+const openai = new OpenAi({
+	apiKey: OPENAI_API_KEY,
+});
 
-app.listen(port, () => console.log(`Running on port ${port}` || 3000 ));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => console.log(`Running on port ${port}` ));
+
+app.post('/create', async (req, res) => {
+	const { prompt } = req.body;
+	try {
+		const response = await openai.images.generate({
+			prompt,
+			n: 1,
+			size: '512x512',
+		});
+		res.send(response.data.data[0].url);
+	}
+	catch (err) {
+		res.send(err.message);
+	}
+});
--- main.js
+++ main.js
@@ -1,50 +1,3 @@
-
-const onSubmit = (e) =>{
-	e.preventDefault();
-	// AFter submit
-	document.querySelector('.text').textContent='';
-	document.querySelector('#image').src = '';
-
-	const prompt = document.querySelector('#prompt').innerText;
-	const size = document.querySelector('#size').value;
-	if(prompt === ''){
-		alert('Please enter some prompt');
-		return;
-	}
-
-	generateImageRequest(prompt, size);
-};
-
-const generateImageRequest = async (prompt, size) =>{
-	try{
-		showSpinner();
-
-		const response = await fetch('./functions/generateImage', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				prompt,
-				size,
-			}),
-		});
-		if(!response.ok){
-			removeSpinner();
-			throw new Error('Image could not be generated ');
-		}
-
-		const data = await response.json();
-
-		const imageUrl = data.data;
-		document.querySelector('#image').src = imageUrl;
-		removeSpinner();
-	}
-	catch(e){
-		document.querySelector('.text').textContent = e.message;
-	}
-};
-
 const showSpinner = () =>{
 	document.querySelector('.spinner').classList.add('show');
 };
@@ -56,6 +9,36 @@ const addLogoClickListener = () =>{
 		location.reload();
 	});
 };
+
+const generateImageRequest = async (prompt) => {
+
+	const response = await axios.post('http://localhost:5500/create', {
+		prompt,
+	});
+
+	console.log(response);
+	const data = await response.json();
+
+	const imageUrl = data.data;
+	document.querySelector('#image').src = imageUrl;
+
+};
+
+const onSubmit = (e) =>{
+	e.preventDefault();
+	// AFter submit
+	document.querySelector('.text').textContent='';
+	document.querySelector('#image').src = '';
+
+	const prompt = document.querySelector('#prompt').innerText;
+	if(prompt === ''){
+		alert('Please enter some prompt');
+		return;
+	}
+
+	generateImageRequest(prompt);
+};
+
 // event listener for the logo
 addLogoClickListener();
 // event listener for the form
